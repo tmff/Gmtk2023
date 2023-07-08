1767dea714c0eceee8de8ed42d46d0cdc3a02328
Meemeboy <44089970+tmff@users.noreply.github.com>
2023-07-08 01:42:25 +0100

    AI

--- Assets/Scripts/Skier.cs
+++ Assets/Scripts/Skier.cs
@@ -12,28 +12,22 @@ public class Skier : MonoBehaviour
 
     public float scaleSize = 1.5f;
 
-    public int obstacleCheckInterval = 5;
-
-    [SerializeField]
-    private bool isDead;
 
     [SerializeField]
     private GameObject snowHead;
 
     [Header("AI")]
-    public float overlapCircleRadius = 15f;
+    public float obstacleCheckInterval = 5;
 
     private enum SteerDirection
     {
         Left,
         Right,
         Straight,
-        Slow
     }
     [SerializeField]
     private SteerDirection currentSteerDirection = SteerDirection.Straight;
 
-    private int directionChoiceAdder = 1;
 
     [SerializeField]
     private bool isOnBlue = true;
@@ -43,6 +37,18 @@ public class Skier : MonoBehaviour
 
     [SerializeField]
     private int flagIndex = 0;
+
+    [SerializeField]
+    private float rightLeftForceMod = 0.2f;
+
+    [SerializeField]
+    private float gravityMod = 0.1f;
+
+
+    [SerializeField]
+    private int rayCastRange = 5;
+    [SerializeField]
+    private int rayAngles = 10;
 
     void Start()
     {
@@ -71,8 +77,8 @@ public class Skier : MonoBehaviour
             rb.AddForce(new Vector2(Random.Range(0.5f,1f),Random.Range(0.5f,1f)) * initialSpeed, ForceMode2D.Impulse);
             rb.AddTorque(100f, ForceMode2D.Impulse);
             GetComponentInChildren<Collider2D>().enabled = false;
+            StopAllCoroutines();
             LeanTween.scale(gameObject, new Vector3(scaleSize,scaleSize,scaleSize), 1f).setLoopPingPong(1).setEaseOutQuad().setOnComplete(() => {
-                isDead = true;
                 rb.velocity = Vector2.zero;
                 rb.angularVelocity = 0f;
                 LeanTween.rotateZ(gameObject, Random.Range(170f,190f), 1f).setEaseOutQuad().setOnComplete(() => {
@@ -89,62 +95,105 @@ public class Skier : MonoBehaviour
     {
         yield return new WaitForSeconds(obstacleCheckInterval);
 
+        float distanceToCurrentFlag;
+        float distanceToNextFlag;
+        Transform currentFlag = isOnBlue ? blueCheckPointParent.transform.GetChild(flagIndex) : redCheckPointParent.transform.GetChild(flagIndex);
+        Transform nextFlag = isOnBlue ? blueCheckPointParent.transform.GetChild(flagIndex + 1) : redCheckPointParent.transform.GetChild(flagIndex + 1);
 
-        //right circle overlap
-        Collider2D[] collidersRight = Physics2D.OverlapCircleAll(transform.position + (transform.right * overlapCircleRadius) + transform.up * -5f, overlapCircleRadius);
-        Debug.Log("Collider right: " + collidersRight.Length);
 
-        Collider2D[] collidersLeft = Physics2D.OverlapCircleAll(transform.position - (transform.right * overlapCircleRadius) + transform.up * -5f, overlapCircleRadius);
-        Debug.Log("Collider left: " + collidersLeft.Length);
 
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up * -1f, 10f,LayerMask.GetMask("Obstacle"));
-        bool hitCheck = hit.collider != null;
-        if(hit.collider != null)
-        {
-            Debug.Log("Hit: " + hit.collider.gameObject.name);
-        }
+
+        RaycastHit2D forwardHit = Physics2D.Raycast(transform.position, rb.velocity.normalized * rayCastRange, rayCastRange,LayerMask.GetMask("Obstacle"));
+        Debug.DrawRay(transform.position, rb.velocity.normalized * rayCastRange, Color.red, 1f);
+        bool forwardHitCheck = forwardHit.collider != null;
+
+        RaycastHit2D leftForwardHit = Physics2D.Raycast(transform.position, rotate(rb.velocity.normalized,Mathf.Deg2Rad * -rayAngles), rayCastRange,LayerMask.GetMask("Obstacle"));
+        Debug.DrawRay(transform.position, rotate(rb.velocity.normalized,Mathf.Deg2Rad * -rayAngles) * rayCastRange, Color.green, 1f);
+        bool leftForwardHitCheck = leftForwardHit.collider != null;
+
+        RaycastHit2D rightForwardHit = Physics2D.Raycast(transform.position, rotate(rb.velocity.normalized,Mathf.Deg2Rad * rayAngles), rayCastRange,LayerMask.GetMask("Obstacle"));
+        Debug.DrawRay(transform.position, rotate(rb.velocity.normalized,Mathf.Deg2Rad * rayAngles) * rayCastRange, Color.blue, 1f);
+        bool rightforwardHitCheck = rightForwardHit.collider != null;
 
         
-        if(collidersRight.Length > collidersLeft.Length + directionChoiceAdder)
+
+        if(forwardHitCheck)
         {
-            currentSteerDirection = SteerDirection.Right;
+            if(isOnBlue && !rightforwardHitCheck)
+            {
+                currentSteerDirection = SteerDirection.Right;
+            }
+            else if(!isOnBlue && !leftForwardHitCheck)
+            {
+                currentSteerDirection = SteerDirection.Left;
+            }
         }
-        else if(collidersLeft.Length > collidersRight.Length + directionChoiceAdder)
+        else if(rightforwardHitCheck)
         {
             currentSteerDirection = SteerDirection.Left;
         }
-        else if(hitCheck)
+        else if(leftForwardHitCheck)
         {
-            currentSteerDirection = SteerDirection.Slow;
+            currentSteerDirection = SteerDirection.Right;
         }
         else
         {
             currentSteerDirection = SteerDirection.Straight;
         }
 
-        //Change target flag
-        //float distanceToCurrentFlag = Vector2.Distance(transform.position, isOnBlue ? blueCheckPointParent.transform.GetChild(flagIndex).position : redCheckPointParent.transform.GetChild(flagIndex).position);
 
+
+        /*
+        if(isOnBlue)
+        {
+
+            distanceToCurrentFlag = Vector2.Distance(transform.position, blueCheckPointParent.transform.GetChild(flagIndex).position);
+            distanceToNextFlag = Vector2.Distance(transform.position, blueCheckPointParent.transform.GetChild(flagIndex + 1).position);
+        }
+        else
+        {
+            distanceToCurrentFlag = Vector2.Distance(transform.position, redCheckPointParent.transform.GetChild(flagIndex).position);
+            distanceToNextFlag = Vector2.Distance(transform.position, redCheckPointParent.transform.GetChild(flagIndex + 1).position);
+        }
+        */
+        distanceToCurrentFlag = Vector2.Distance(transform.position, currentFlag.position);
+        distanceToNextFlag = Vector2.Distance(transform.position, nextFlag.position);
+        if(distanceToNextFlag < distanceToCurrentFlag || transform.position.y < currentFlag.position.y)
+        {
+            flagIndex++;
+            if(flagIndex >= (isOnBlue ? blueCheckPointParent.transform.childCount : redCheckPointParent.transform.childCount))
+            {
+                //Finish skier
+                Debug.Log("Finish skier");
+            }
+        }
         StartCoroutine(CheckForObstacles());
     }
 
 
     void Propel()
     {
+        rb.AddForce(transform.up * initialSpeed * -1f * gravityMod, ForceMode2D.Impulse);
+
         Debug.Log("Propel: " + currentSteerDirection);
         switch(currentSteerDirection)
         {
             case SteerDirection.Left:
-                rb.AddForce(transform.right * -1f * initialSpeed / 5f, ForceMode2D.Impulse);
+                rb.AddForce(transform.right * -1f * initialSpeed * rightLeftForceMod, ForceMode2D.Impulse);
                 break;
             case SteerDirection.Right:
-                rb.AddForce(transform.right * initialSpeed / 5f, ForceMode2D.Impulse);
-                break;
-            case SteerDirection.Slow:
-                rb.AddForce(transform.up * initialSpeed * 0.5f, ForceMode2D.Impulse);
+                rb.AddForce(transform.right * initialSpeed * rightLeftForceMod, ForceMode2D.Impulse);
                 break;
             case SteerDirection.Straight:
-                //rb.AddForce(transform.up * initialSpeed, ForceMode2D.Impulse);
+                //Add force towards next flag
+                if(isOnBlue)
+                {
+                    rb.AddForce((blueCheckPointParent.transform.GetChild(flagIndex).position - transform.position).normalized * initialSpeed / 2f, ForceMode2D.Impulse);
+                }
+                else
+                {
+                    rb.AddForce((redCheckPointParent.transform.GetChild(flagIndex).position - transform.position).normalized * initialSpeed / 2f, ForceMode2D.Impulse);
+                }
                 break;
         }
     }
@@ -155,6 +204,14 @@ public class Skier : MonoBehaviour
         Propel();
         StartCoroutine(PropelRoutine());
     }
+
+    public Vector2 rotate(Vector2 v, float delta) {
+        return new Vector2(
+            v.x * Mathf.Cos(delta) - v.y * Mathf.Sin(delta),
+            v.x * Mathf.Sin(delta) + v.y * Mathf.Cos(delta)
+        );
+    }
+
 
 
 }
